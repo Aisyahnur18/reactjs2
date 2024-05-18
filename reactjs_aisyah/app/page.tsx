8a1a065b31b251b959af6a6e7b4c5893fa262c8c
Aisyah Nur Hasunah <22572005@stmik.jayakarta.ac.id>
2024-05-18 16:30:54 +0700

    first commit

---
+++ reactjs_aisyah/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{ fontSize: "24px", color: "yellow" }}>home movie list</h1>
+      <h2 style={{ fontSize: "28px", color: "white" }}>Aisyah Nur Hasunah</h2>
+      <h3 style={{ fontSize: "16px", color: "green" }}>kelas pemrograman web 2023-2024</h3>
+    </div>
+  );
+}
